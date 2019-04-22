62afa7ca544bda9af0cac9365e1de6b86bbbcfe6
autom8ter <admin@autom8ter.com>
2019-04-22 16:39:22 -0600

    validate

--- go/api/api.pb.util.go
+++ go/api/api.pb.util.go
@@ -2,6 +2,14 @@ package api
 
 import (
 	"github.com/autom8ter/objectify"
+	"github.com/pkg/errors"
 )
 
 var Util = objectify.Default()
+
+func (p *Profile) Validate() error {
+	if p.Name == "" {
+		return errors.New("Validate: a profile name is required")
+	}
+	return nil
+}
