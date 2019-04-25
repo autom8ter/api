54b6e0f5bd96c05755081fb6dfb0ca92244c1f81
autom8ter <admin@autom8ter.com>
2019-04-24 23:33:32 -0600

    context

--- util.go
+++ util.go
@@ -1,7 +1,16 @@
 package api
 
 import (
+	"context"
 	"github.com/autom8ter/objectify"
+	"os"
 )
 
+func init() {
+	Context = context.WithValue(context.TODO(), "env", os.Environ())
+}
+
 var Util = objectify.Default()
+
+var Context context.Context
+
